6543107d40887213d9080816a46166f399b45c85
fangxinmiao <smecf@163.com>
2017-08-25 14:39:09 +0800

    清理代码

--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -2,19 +2,11 @@ package io.renren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-
 
 @SpringBootApplication
-public class RenrenApplication extends SpringBootServletInitializer {
+public class RenrenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RenrenApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(RenrenApplication.class);
     }
 }
